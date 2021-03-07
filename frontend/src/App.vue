dea5a7265349477a5486c96457cc8876317718d5
Tarcisio Nunes <github@tarcnux.com.br>
2021-03-07 02:22:26 -0300

    Fix: Reload page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand navbar-light bg-warning">
-      <router-link to="/" class="navbar-brand">tarcnux</router-link>
+    <nav class="navbar navbar-expand navbar-light" style="background-color:#e77924">
+      <router-link to="/" class="navbar-brand">Catálogo</router-link>
       <div class="navbar-nav mr-auto">
         <li class="nav-item">
-          <router-link to="/parts" class="nav-link">Listar Peças</router-link>
+          <!-- <router-link to="/parts" class="nav-link" style="font-size:1.3em">
+            Listar Peças
+          </router-link> -->
+          <a href="/parts" class="nav-link" style="font-size:1.3em" >
+            Listar Peças
+          </a>
         </li>
         <li class="nav-item">
-          <router-link to="/add" class="nav-link">Cadastrar nova peça</router-link>
+          <router-link to="/add" class="nav-link" style="font-size:1.3em">
+            Cadastrar peça
+          </router-link>
         </li>
       </div>
     </nav>
