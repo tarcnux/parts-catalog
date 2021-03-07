b6735aac7f50c0fa383b891c619cdae7ff31efd4
Tarcisio Nunes <github@tarcnux.com.br>
2021-03-07 00:12:46 -0300

    Closes #10 - Create Data Service

---
+++ frontend/src/services/PartDataService.js
@@ -0,0 +1,37 @@
+import http from "../http-common";
+
+class TutorialDataService {
+    getAll() {
+      return http.get("/parts");
+    }
+  
+    get(id) {
+      return http.get(`/parts/${id}`);
+    }
+  
+    create(data) {
+      return http.post("/parts", data);
+    }
+  
+    update(id, data) {
+      return http.put(`/parts/${id}`, data);
+    }
+  
+    delete(id) {
+      return http.delete(`/parts/${id}`);
+    }
+  
+    deleteAll() {
+      return http.delete(`/parts`);
+    }
+  
+    findByName(name)  {
+      return http.get(`/parts?name=${name}`);
+    }
+
+    findByVehicle(vehicle)  {
+        return http.get(`/parts?vehicle=${vehicle}`);
+      }
+  }
+  
+  export default new TutorialDataService();
