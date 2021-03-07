e2b96ac883c4d0abb5bdd64489bed21b6b525eea
Tarcisio Nunes <github@tarcnux.com.br>
2021-03-07 00:03:20 -0300

    closes #8 - Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand navbar-light bg-warning">
+      <router-link to="/" class="navbar-brand">tarcnux</router-link>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/parts" class="nav-link">Listar Peças</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/add" class="nav-link">Cadastrar nova peça</router-link>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container mt-3">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
